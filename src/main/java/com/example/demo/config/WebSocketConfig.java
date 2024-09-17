54a15b4cbda1018ca79e39f28af663c0ddcc34c7
ebtasamfaridy <33486311+ebtasamfaridy@users.noreply.github.com>
2024-09-18 04:30:55 +0530

    Create WebSocketConfig.java

---
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -0,0 +1,21 @@
+package com.example.demo.config;
+import com.example.demo.handler.MyWebSocketHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myWebSocketHandler(), "/websocket").setAllowedOrigins("*");
+    }
+    @Bean
+    public MyWebSocketHandler myWebSocketHandler() {
+        return new MyWebSocketHandler();
+    }
+}
